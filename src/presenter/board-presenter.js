Shapovaloff/taskmanager-17

5.1.5 Реализует логику обновления задачи
- Опишем в презентере доски обработчик изменений в задаче - #handleTaskChange
- После обновления данных повторно инициализируем Task-презентер уже с новыми данными

src/presenter/board-presenter.js
@@ -5,6 +5,7 @@ import TaskListView from '../view/task-list-view.js';
 import LoadMoreButtonView from '../view/load-more-button-view.js';
 import NoTaskView from '../view/no-task-view.js';
 import TaskPresenter from './task-presenter.js';
+import {updateItem} from '../utils/common.js';
 
 const TASK_COUNT_PER_STEP = 8;
 
@@ -40,6 +41,11 @@ export default class BoardPresenter {
     if (this.#renderedTaskCount >= this.#boardTasks.length) {
       remove(this.#loadMoreButtonComponent);
     }
+  };
+
+  #handleTaskChange = (updatedTask) => {
+    this.#boardTasks = updateItem(this.#boardTasks, updatedTask);
+    this.#taskPresenter.get(updatedTask.id).init(updatedTask);
   };
 
   #renderSort = () => {

htmlacademy-ecmascript/taskmanager-20

src/presenter/board-presenter.js
@@ -5,6 +5,7 @@ import TaskListView from '../view/task-list-view.js';
 import LoadMoreButtonView from '../view/load-more-button-view.js';
 import NoTaskView from '../view/no-task-view.js';
 import TaskPresenter from './task-presenter.js';
+import {updateItem} from '../utils/common.js';
 
 const TASK_COUNT_PER_STEP = 8;
 
@@ -39,6 +40,11 @@ export default class BoardPresenter {
     if (this.#renderedTaskCount >= this.#boardTasks.length) {
       remove(this.#loadMoreButtonComponent);
     }
+  };
+
+  #handleTaskChange = (updatedTask) => {
+    this.#boardTasks = updateItem(this.#boardTasks, updatedTask);
+    this.#taskPresenters.get(updatedTask.id).init(updatedTask);
   };
 
   #renderSort() {